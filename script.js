cfbb7c0e698f77ce019d00ca8aa48167b049c61f
frdrk00 <masterkooo@gmail.com>
2023-07-21 19:52:39 +0200

    Final Version_01

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const pieColors = ['#565656', '#9D4343', '#565656', '#9D4343', '#565656', '#9D4343', '#565656', '#9D4343']
+
 let labels = []
+const defaultLabels = [100 + "$", 50 + "$", 0 + "$", 10 + "$", "present", 0 + "$" , 1000 + "$", 75 + "$"]
+const data = [16, 16, 16, 16, 16, 16, 16, 16]
 let count = 0
 let resultValue = 101
-const data = [16, 16, 16, 16, 16, 16]
-const pieColors = ['#9D4343', '#565656']
+
 const rotationValues = [
   { minDegree: 0, maxDegree: 30, value: 2 },
   { minDegree: 31, maxDegree: 90, value: 1 },
@@ -13,18 +16,17 @@ const rotationValues = [
   { minDegree: 331, maxDegree: 360, value: 2 },
 ]
 
+const wheel = document.getElementById('wheel')
 const addButton = document.getElementById('add-button')
 const spinBtn = document.getElementById('spin-btn')
 const inputElement = document.getElementById('input')
-const wheel = document.getElementById('wheel')
-const spinnerElement = document.querySelector('#wheel')
 const finalValue = document.getElementById('final-value')
 
-function renderOptions() {
+const renderOptions = () => {
   const optionsElement = document.getElementById('options')
   optionsElement.innerHTML = ''
 
-  labels.forEach(function (label, index) {
+  labels.forEach((label, index) => {
     const optionElement = document.createElement('div')
     optionElement.classList.add('option')
 
@@ -35,7 +37,7 @@ function renderOptions() {
     const deleteButton = document.createElement('button')
     deleteButton.classList.add('delete-button')
     deleteButton.innerText = 'X'
-    deleteButton.onclick = function () {
+    deleteButton.onclick = () => {
       deleteOption(index)
     }
 
@@ -45,50 +47,38 @@ function renderOptions() {
   })
 }
 
-function deleteOption(index) {
+const deleteOption = (index) => {
   labels.splice(index, 1)
   renderOptions()
 }
 
-function addButtonClick(e) {
-  const input = document.getElementById('input')
-  const value = input.value.trim()
-
+addButton.addEventListener('click', () => {
+  const value = inputElement.value.trim()
   if (value !== '') {
     labels.push(value)
-    input.value = ''
+    inputElement.value = ''
     renderOptions()
 
     const inputLabels = document.getElementById('input')
     inputLabels.value = labels.join(', ')
-
   }
+})
+
+const updateSpinner = (value) => {
+  wheel.textContent = value
 }
 
-addButton.addEventListener('click', addButtonClick)
-
-function updateSpinner(value) {
-  spinnerElement.textContent = value
-}
-
-function handleInputChange() {
+const handleInputChange = () => {
   updateSpinner(inputElement.value)
 }
 
 inputElement.addEventListener('input', handleInputChange)
-addButton.addEventListener('click', function () {
-  const value = inputElement.value.trim()
-  if (value !== '') {
-    renderOptions()
-    updateSpinner(value)
-  }
-})
 
 const myChart = new Chart(wheel, {
   plugins: [ChartDataLabels],
   type: 'pie',
   data: {
-    labels: [1, 2, 3, 4, 5, 6],
+    labels: defaultLabels,
     datasets: [
       {
         backgroundColor: pieColors,
@@ -117,24 +107,27 @@ const myChart = new Chart(wheel, {
 })
 
 function updateRotationValues() {
-  for (let i = 0; i < rotationValues.length; i++) {
-    rotationValues[i].value = labels[i] || 0
+  for (const [i, label] of labels.entries()) {
+    rotationValues[i].value = label || 0
   }
 }
 
 addButton.addEventListener('click', () => {
   const inputLabelsValue = inputElement.value
-  labels = inputLabelsValue.split(',').map((label) => label.trim())
+  labels = inputLabelsValue
+    .split(',')
+    .map((label) => label.trim())
+    .filter((label) => label !== '')
   myChart.data.labels = labels.length ? labels : []
 
   updateRotationValues()
-  inputElement.value = '';
+  inputElement.value = ''
 
   myChart.update()
 })
 
 const valueGenerator = (angleValue) => {
-  for (let i of rotationValues) {
+  for (const i of rotationValues) {
     if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
       finalValue.innerHTML = `<p>Previous prize : ${i.value}</p>`
       spinBtn.disabled = false
